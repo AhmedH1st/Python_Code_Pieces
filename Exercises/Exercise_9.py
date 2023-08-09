78f7533cc5b6de1df5187396c90e87912d789485
AhmedH1st <ahmedhesham1st@gmail.com>
2023-08-09 22:31:53 +0300

    labs

---
+++ Exercises/Exercise_9.py
@@ -0,0 +1,20 @@
+#script to print All System Info 
+import platform
+import psutil
+
+#print OS running type
+print("Platform :"+platform.platform())
+#print processor type
+print("Processor :"+platform.processor())
+#print architectural detail
+print("architecture :",platform.architecture(),"\n")
+#print Memory Size & Usage
+print("Memory :",psutil.virtual_memory(),"\n")
+#print PC IPv4, IPv6, Mac Address
+print("Network info :",psutil.net_if_addrs(),"\n")
+#print PC Internal Temperature
+print("Temp :",psutil.sensors_temperatures(),"\n")
+
+
+
+#for more APIs visit https://pypi.org/project/psutil/
