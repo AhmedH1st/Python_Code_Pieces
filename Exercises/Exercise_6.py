8d7652afba9c9bb7d25e03583efce66499e2c3bb
AhmedH1st <ahmedhesham1st@gmail.com>
2023-08-09 18:59:48 +0300

    new tasks

---
+++ Exercises/Exercise_6.py
@@ -0,0 +1,18 @@
+
+
+#Python Code that Handles Login System
+
+login_sys = {
+    "Ahmed": "1394",
+    "Ali" : "6078",
+    "Amr" : "9345"
+}
+
+UserName = input("Enter Your User name : ")
+Password = input("Enter Your Password : " )
+
+if login_sys.get(UserName) == Password:
+    print("Welcome Sir")
+else:
+    print("Access Denied")
+    
